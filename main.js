5a01786f7768d4efd8162dd20fa363138f3f4201
nayesha110 <79211794+nayesha110@users.noreply.github.com>
2021-02-17 21:13:54 +0530

    my first filter app mustache

--- main.js
+++ main.js
@@ -1,43 +1,40 @@
+noseX=0;
+noseY=0;
+
 function preload() {
-    clown_nose=loadImage('https://i.postimg.cc/664kZfW9/clown-nose.png');
-    
+  clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup() {
-    canvas=createCanvas(400,400);
-    canvas.position(470,210);
-    video=createCapture(VIDEO)
-    video.hide();
-    poseNet=ml5.poseNet(video,modelLoaded);//initializing pose net model
-    poseNet.on('pose',gotPoses);
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
 
-     }
-
-     function gotPoses(results) {
-     if (results.length>0) {
-         console.log(results);
-         console.log("nosex="+results[0].pose.nose.x);
-         console.log("nosey="+results[0].pose.nose.y);
-         
-
-     }
-         
-     }
-
-
-function draw() {
-image(video,0,0,400,400)
-fill(252, 44, 3)
-stroke(252, 44, 3)
-circle(nosex,nosey,20)
-image(close_nose,nosex-15,nosey-15,30,30)
-}
-
-function take_snapshot() {
-    save('nayesha_pic.png');
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
-    console.log( "poseNet model is initiatliezed")
-    
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-40;
+    noseY = results[0].pose.nose.y;
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 80, 35);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
 }
